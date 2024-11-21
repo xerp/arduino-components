f95958f7d2d08521e031f2ee181b50d335eaa1c0
Santiago de Pedro <santiago.pedro23@gmail.com>
2024-11-21 17:07:19 -0400

    Remove unused arch directory and integrate base.cpp
    Deleted the arch/CMakeLists.txt file and moved base.cpp to src. Updated include paths and included previously commented-out functions in base.cpp for proper operation.

--- arch/base.cpp
+++ arch/base.cpp
@@ -1,5 +1,5 @@
 #include "arch/base.h"
-//#include "arch/base-internal.h"
+#include "arch/base-internal.h"
 
 void board_init(char name[]) {
     board->name = name;
@@ -31,44 +31,44 @@ void board_set_delay_function(void (*delayFunction)(ulong_t)) {
 
 // Internal Functions
 
-//void board_set_pin_mode(uint8_t pin, uint8_t mode) {
-//    if (board->pinMode != None) {
-//        board->pinMode(pin, mode);
-//    }
-//}
-//
-//void board_set_digital_write(uint8_t pin, uint8_t value) {
-//    if (board->digitalWrite != None) {
-//        board->digitalWrite(pin, value);
-//    }
-//}
-//
-//int32_t board_get_digital_read(uint8_t pin) {
-//    if (board->digitalRead != None) {
-//        return board->digitalRead(pin);
-//    }
-//
-//    return NoValue;
-//}
-//
-//ulong_t board_get_millis() {
-//    if (board->millis != None) {
-//        return board->millis();
-//    }
-//
-//    return NoValue;
-//}
-//
-//ulong_t board_get_micros() {
-//    if (board->micros != None) {
-//        return board->micros();
-//    }
-//
-//    return NoValue;
-//}
-//
-//void board_delay(ulong_t value) {
-//    if (board->delay != None) {
-//        board->delay(value);
-//    }
-//}
+void board_set_pin_mode(uint8_t pin, uint8_t mode) {
+    if (board->pinMode != None) {
+        board->pinMode(pin, mode);
+    }
+}
+
+void board_set_digital_write(uint8_t pin, uint8_t value) {
+    if (board->digitalWrite != None) {
+        board->digitalWrite(pin, value);
+    }
+}
+
+int32_t board_get_digital_read(uint8_t pin) {
+    if (board->digitalRead != None) {
+        return board->digitalRead(pin);
+    }
+
+    return NoValue;
+}
+
+ulong_t board_get_millis() {
+    if (board->millis != None) {
+        return board->millis();
+    }
+
+    return NoValue;
+}
+
+ulong_t board_get_micros() {
+    if (board->micros != None) {
+        return board->micros();
+    }
+
+    return NoValue;
+}
+
+void board_delay(ulong_t value) {
+    if (board->delay != None) {
+        board->delay(value);
+    }
+}

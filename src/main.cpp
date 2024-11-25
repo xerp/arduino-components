94fa1aa4ec48525e25e2a0da2fc64c89b7831367
Santiago de Pedro <santiago.pedro23@gmail.com>
2024-11-25 11:42:33 -0400

    Modify button pin and adjust printf formatting.
    Changed button pin initialization to 13 and updated printf statements for better readability. Also, adjusted return value in the get_digital_read_callback function and moved the board name print statement for a more logical order.

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,14 @@
 
 using Devices::Button;
 
-Button button(1);
+Button button(13);
 
-void pin_mode_callback(uint8_t pin,  uint8_t mode) {
-    printf("Pin :%d mode :%d\n", pin, mode);
+void pin_mode_callback(uint8_t pin, uint8_t mode) {
+    printf("Pin: %d mode: %d\n", pin, mode);
 }
 
 int32_t get_digital_read_callback(uint8_t pin) {
-    printf("Pin :%d\n", pin);
-
-    return 0;
+    return 1;
 }
 
 int main(int argc, char **argv) {
@@ -21,11 +19,11 @@ int main(int argc, char **argv) {
     board_set_pin_mode_function(pin_mode_callback);
     board_set_digital_read_function(get_digital_read_callback);
 
+    printf("Board name: %s\n", board_get_name());
     button.begin();
-    printf("%s\n", board_get_name());
 
     button.startLoop();
-    printf("status :%d\n", button.statusChanged());
+    printf("status: %d\n", button.statusChanged());
     sleep(2);
 
     return 0;
